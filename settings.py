a236158f9f39ae87622932d8fe8e5e08c2913057
YASSINE-AA <65458854+YASSINE-AA@users.noreply.github.com>
2020-05-16 17:31:15 +0100

    Update settings.py

--- settings.py
+++ settings.py
@@ -13,6 +13,7 @@ blue = (0, 0, 255)
 grey = (128,128,128)
 material_blue = (3,169,244)
 white = (255, 255, 255)
+
 # Grid 
 grid = (38, 150, 504, 504)
 
@@ -20,7 +21,6 @@ grid = (38, 150, 504, 504)
 used_font = 'freesansbold.ttf'
 
 # Authors 
-
 authors = {
     'Gui':'Yassine Ahmed Ali, Mustapha Ahmed Ali',
     'Algorithm':'Yassine Ahmed Ali'
